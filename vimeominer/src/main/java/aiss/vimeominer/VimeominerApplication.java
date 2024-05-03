7436852804df38bd608dc7a57c36e35b518901dd
Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
2024-05-03 10:58:08 +0200

    add vimeominer & youtubeminer

---
+++ vimeominer/src/main/java/aiss/vimeominer/VimeominerApplication.java
@@ -0,0 +1,13 @@
+package aiss.vimeominer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeominerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeominerApplication.class, args);
+	}
+
+}
---
+++ youtubeminer/src/test/java/aiss/youtubeminer/YoutubeminerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss.youtubeminer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeminerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
