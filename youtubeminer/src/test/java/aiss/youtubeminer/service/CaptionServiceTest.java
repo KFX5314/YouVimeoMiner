b2492aee017005833ba06c553ee79a350c339447
Álvaro Luque <alvluqbuz@alum.us.es>
2024-05-09 19:04:37 +0200

    cambio menor en test

--- youtubeminer/src/test/java/aiss/youtubeminer/service/CaptionServiceTest.java
+++ youtubeminer/src/test/java/aiss/youtubeminer/service/CaptionServiceTest.java
@@ -20,8 +20,8 @@ class CaptionServiceTest {
     @DisplayName("Get captions from a given video")
     void getVideosFromChannel() {
         // video 275VkeAlu90
-        List<Caption> Captions = service.getCaptionsForVideo("275VkeAlu90", "API_KEY");
-        assertFalse(Captions.isEmpty(), "The list of Captions is empty");
-        System.out.println(Captions.stream().map(c -> c.getSnippet().getLanguage()).toList());
+        List<Caption> captions = service.getCaptionsForVideo("275VkeAlu90", "API_KEY");
+        assertFalse(captions.isEmpty(), "The list of Captions is empty");
+        System.out.println(captions.stream().map(c -> c.getSnippet().getLanguage()).toList());
     }
 }
