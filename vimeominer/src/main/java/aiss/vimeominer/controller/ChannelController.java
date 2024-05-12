569f940ae0dfaf02c19eb3a15b8751e6e1920c1d
pabniecor <pabniecor@alum.us.es>
2024-05-12 20:51:30 +0200

    Documentacion Automática de Vimeominer

--- vimeominer/src/main/java/aiss/vimeominer/controller/ChannelController.java
+++ vimeominer/src/main/java/aiss/vimeominer/controller/ChannelController.java
@@ -6,9 +6,17 @@ import aiss.vimeominer.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+@Tag(name = "Channel", description = "Channel management API")
 @RestController
 @RequestMapping("/vimeominer")
 
@@ -28,9 +36,24 @@ public class ChannelController
     // GET http://localhost:8081/vimeominer/channels/{channelId}
     // EJEMPLO DE GET: http://localhost:8081/vimeominer/channels/sundanceshorts?maxVideos=10&maxComments=10
 
+    @Operation( summary = "Retrieve a Channel by Id",
+            description = "By default the channel comes with 10 videos (if possible), and each video with 10 comments and captions (if possible), the limit of comments and videos can be passed as parameters",
+            tags = { "channels", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Retrieved channel",
+                    content = { @Content(schema = @Schema(implementation = Channel.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Channel not found",
+                    content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/channels/{channelId}")
-    public Channel getChannel(@PathVariable(value="channelId") String channelId,
+    public Channel getChannel(@Parameter(description = "id of channel to be searched")
+                                  @PathVariable(value="channelId") String channelId,
+                              @Parameter(description = "limit of videos to add to the channel")
                                           @RequestParam(defaultValue = "10") int maxVideos,
+                              @Parameter(description = "limit of comments to add to the channel's videos")
                                           @RequestParam(defaultValue = "10") int maxComments
 
     ) throws ChannelNotFoundException {
@@ -51,12 +74,27 @@ public class ChannelController
     }
 
     // POST http://localhost:8082/vimeominer/channels/{channelId}
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Channel created",
+                    content = {@Content(schema = @Schema(implementation = aiss.vimeominer.videominer_models.Channel.class),
+                            mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "404", description = "Channel not found",
+                    content = { @Content(schema = @Schema()) })
+    })
+    @Operation( summary = "Gets the channel information from YouTube's API using the Id passed as parameter and posts it into the database",
+            description = "Copies a channel getting the information from YouTube's API",
+            tags = { "channels", "post" })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/channels/{channelId}")
     public aiss.vimeominer.videominer_models.Channel downloadChannel(
-            @PathVariable(value = "channelId") String channelId,
-            @RequestParam(defaultValue = "10") Integer maxVideos,
-            @RequestParam(defaultValue = "10") Integer maxComments
+            @Parameter(description = "id of channel to be searched")
+                @PathVariable(value = "channelId") String channelId,
+            @Parameter(description = "limit of videos to add to the channel")
+                @RequestParam(defaultValue = "10") Integer maxVideos,
+            @Parameter(description = "limit of comments to add to the channel's videos")
+                @RequestParam(defaultValue = "10") Integer maxComments
     ) throws ChannelNotFoundException {
         Channel channel = getChannel(channelId, maxVideos, maxComments);
 
